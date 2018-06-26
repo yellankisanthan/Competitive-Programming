e7ffc6938f38d20b497513dfbd71b3eff32d5037
yellankisanthan <31200172+yellankisanthan@users.noreply.github.com>
2018-06-26 17:09:02 +0530

    Add files via upload

---
+++ Competitive-Programming/Week-1/Day-1/Product_of_all_others.py
@@ -0,0 +1,67 @@
+import unittest
+
+
+def get_products_of_all_ints_except_at_index(input_list):
+
+    if len(input_list) <= 1:
+        raise ValueError('Required Atleast 2 Elements')
+
+    productList = [None] * len(input_list)
+
+    product = 1
+    for i in range(0,len(input_list)):
+        productList[i] = product
+        product *= input_list[i]
+
+    product = 1
+    for i in range(len(input_list) - 1, -1, -1):
+        productList[i] *= product
+        product *= input_list[i]
+
+    return productList
+    
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_small_list(self):
+        actual = get_products_of_all_ints_except_at_index([1, 2, 3])
+        expected = [6, 3, 2]
+        self.assertEqual(actual, expected)
+
+    def test_longer_list(self):
+        actual = get_products_of_all_ints_except_at_index([8, 2, 4, 3, 1, 5])
+        expected = [120, 480, 240, 320, 960, 192]
+        self.assertEqual(actual, expected)
+
+    def test_list_has_one_zero(self):
+        actual = get_products_of_all_ints_except_at_index([6, 2, 0, 3])
+        expected = [0, 0, 36, 0]
+        self.assertEqual(actual, expected)
+
+    def test_list_has_two_zeros(self):
+        actual = get_products_of_all_ints_except_at_index([4, 0, 9, 1, 0])
+        expected = [0, 0, 0, 0, 0]
+        self.assertEqual(actual, expected)
+
+    def test_one_negative_number(self):
+        actual = get_products_of_all_ints_except_at_index([-3, 8, 4])
+        expected = [32, -12, -24]
+        self.assertEqual(actual, expected)
+
+    def test_all_negative_numbers(self):
+        actual = get_products_of_all_ints_except_at_index([-7, -1, -4, -2])
+        expected = [-8, -56, -14, -28]
+        self.assertEqual(actual, expected)
+
+    def test_error_with_empty_list(self):
+        with self.assertRaises(Exception):
+            get_products_of_all_ints_except_at_index([])
+
+    def test_error_with_one_number(self):
+        with self.assertRaises(Exception):
+            get_products_of_all_ints_except_at_index([1])
+
+
+unittest.main(verbosity=2)
